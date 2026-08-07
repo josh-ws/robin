4d4c7ce5de69257581648ac3cbf3f077df5ed851
Josh Wray-Sparks <178158934+josh-ws@users.noreply.github.com>
2026-08-07 14:00:45 +0100

    basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, BufRead, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut lines = io::stdin().lock().lines();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        match lines.next() {
+            Some(Ok(line)) => handle_line(&line),
+            Some(Err(e)) => panic!("input error"),
+            None => break,
+        }
+    }
+}
+
+fn handle_line(line: &str) {
+    println!("{}", line)
 }
